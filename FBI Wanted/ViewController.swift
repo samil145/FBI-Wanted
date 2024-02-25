71a9a01eac1b073a3915ac1ecd808ff7d5b26cec
samil145 <samil.bayramli.sb@gmail.com>
2024-02-25 14:27:31 +0400

    Initial Commit

---
+++ FBI Wanted/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FBI Wanted
+//
+//  Created by Shamil Bayramli on 25.02.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
